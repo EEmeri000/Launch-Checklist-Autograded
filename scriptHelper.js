d573dd460442639e16ffe3c6f7c1442fab73183d
CannibalCrab <138933689+EEmeri000@users.noreply.github.com>
2023-08-10 17:40:45 -0400

    All tests passing, readjusted variable references

--- scriptHelper.js
+++ scriptHelper.js
@@ -54,21 +54,21 @@ let launchStatus = document.getElementById("launchStatus")
         launchStatus.innerHTML = 'Shuttle Not Ready for Launch';
         fuelStatus.innerHTML = 'Fuel level too low for launch';
     } else if (cargoLevel > 10000) {
-        faultyItems.style.visibility = 'visible';
+        list.style.visibility = 'visible';
         launchStatus.style.color = 'red';
         launchStatus.innerHTML = 'Shuttle Not Ready for Launch';
         fuelStatus.innerHTML = "Fuel level high enough for launch";
         cargoStatus.innerHTML = 'Cargo mass too heavy for launch';
-    } else if (fuelLevel < 10000 && cargoLevel > 10000) {
-        cargoStatus.innerHTML = 'Cargo mass too heavy for launch';
-        fuelStatus.innerHTML = 'Fuel level too low for launch';
-        list.style.visibility ='visible';
-        launchStatus.innerHTML ='Shuttle Not Ready for Launch';
-        launchStatus.style.color = 'red';
+    // } else if (fuelLevel < 10000 && cargoLevel > 10000) {
+    //     cargoStatus.innerHTML = 'Cargo mass too heavy for launch';
+    //     fuelStatus.innerHTML = 'Fuel level too low for launch';
+    //     list.style.visibility ='visible';
+    //     launchStatus.innerHTML ='Shuttle Not Ready for Launch';
+    //     launchStatus.style.color = 'red';
     } else {
-        faultyItems.style.visibility = 'visible';
-        h2.style.color = 'green';
-        h2.innerHTML = 'Shuttle is Ready for Launch';
+        list.style.visibility = 'visible';
+        launchStatus.style.color = 'green';
+        launchStatus.innerHTML = 'Shuttle is Ready for Launch';
         fuelStatus.innerHTML = "Fuel level high enough for launch";
         cargoStatus.innerHTML= "Cargo mass low enough for launch";
     }
