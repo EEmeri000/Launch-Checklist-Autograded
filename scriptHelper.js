35b46fce64688e09dca17b2ff27c1f140e64312f
CannibalCrab <138933689+EEmeri000@users.noreply.github.com>
2023-08-10 12:34:39 -0400

    small changes

--- scriptHelper.js
+++ scriptHelper.js
@@ -5,16 +5,17 @@ require('cross-fetch/polyfill');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
     // Here is the HTML formatting for our mission target div.
-    /*
+    let missionTarget = document.getElementById("missionTarget");
+    /* missionTarget.innerHTML =
                  <h2>Mission Destination</h2>
                  <ol>
-                     <li>Name: </li>
-                     <li>Diameter: </li>
+                     <li>Name: ${name}</li>
+                     <li>Diameter: ${diameter}</li>
                      <li>Star: ${star}</li>
-                     <li>Distance from Earth: </li>
-                     <li>Number of Moons: </li>
+                     <li>Distance from Earth: ${distance}</li>
+                     <li>Number of Moons: ${moons}</li>
                  </ol>
-                 <img src="">
+                 <img src="${imageUrl}" alt="Mission Destination Image">
     */
  }
  
@@ -71,6 +72,7 @@ let faultyItems = document.getElementById("faultyItems")
      let planetsReturned;
  
      planetsReturned = await fetch().then( function(response) {
+        return response.json();
          });
  
      return planetsReturned;
